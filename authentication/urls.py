9f32e8695ba95aa33899fbf7ae0406a8e74583e5
Shahin Kalathil <shahinluttu@gmail.com>
2025-06-16 16:50:36 +0530

    login_pages

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.Index, name='index'),
     path('sign-up',views.Sign_Up, name='sign_up'),
+    path('otp-validation',views.Otp_Validation, name='otp_validation'),
     path('sign-in',views.Sign_In, name='sign_in'),
-    path('forgot-password',views.Forgot_Password, name='forgot_password'),
+    path('sign-in/forgot-password',views.Forgot_Password, name='forgot_password'),
+    path('sign-in/emil-otp-veification',views.Email_Otp_Varification, name='email_otp_verification'),
+    path('sign-in/reset-password', views.Reset_Password, name='reset_password')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -4,13 +4,22 @@ from django.shortcuts import render
 def Index(request):
     return render(request, 'user/index.html')
 
-
 def Sign_Up(request):
-    return render(request, 'user/sign_up.html')
+    return render(request, 'user/sign_up.html',{'hide_layout': True,})
+    
 
+def Otp_Validation(request):
+    return render(request, 'user/otp_validation.html',{'hide_layout': True,})
 
 def Sign_In(request):
-    return render(request, 'user/sign_in.html')
+    return render(request, 'user/sign_in.html',{'hide_layout': True,})
 
 def Forgot_Password(request):
-    return render(request, 'user/forgot_password.html')
+    return render(request, 'user/forgot_password.html',{'hide_layout': True,})  
+
+
+def Email_Otp_Varification(request):
+    return render(request,'user/email_otp_varification.html',{'hide_layout': True,})
+
+def Reset_Password(request):
+    return render(request, 'user/reset_password.html',{'hide_layout': True,})
